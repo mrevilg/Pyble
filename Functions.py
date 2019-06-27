8b59b72c94f5f4beaa3f6260014dc7e03f806db4
Stephen Clements <mrevilg@gmail.com>
2019-06-27 14:44:06 -0500

    Inital Function Data

--- Functions.py
+++ Functions.py
@@ -34,3 +34,29 @@ completed_models = []
 # Here, [:] indactes a copy of the list
 print_models(unprinted_designs[:], completed_models)
 show_completed_models(completed_models)
+
+
+# Passing an arbitrary number of arguments
+
+# the '*' tells Py to create an empty tuple
+def make_pizza(*toppings):
+    """Summarize the pizza we're about to make"""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', "green peppers", "corn")
+
+
+# Mixing positional and arbitrary arguments
+
+def make_pizza(size, *toppings):
+    """Summarize the pizza we're about to make"""
+    print("\nMaking a " +str(size) + 
+        "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', "green peppers", "corn")
