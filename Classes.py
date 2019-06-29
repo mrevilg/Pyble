06ca16233c72b20122f2616bbbff1c77cda85f50
Stephen Clements <mrevilg@gmail.com>
2019-06-29 14:07:07 -0500

    Secondary Class information

--- Classes.py
+++ Classes.py
@@ -35,3 +35,34 @@ print("My dog is " + str(your_dog.age) + " year's old.")
 your_dog.roll_over()
 
 # Each instance must occupy its own space in memory
+
+
+# Clean class based on Resturants
+class Resturant():
+    """A simple model of an eatery"""
+
+    def __init__(self, name, cuisine):
+        """Initialize name and cuisine attributes"""
+        self.name = name
+        self.cuisine = cuisine
+    
+    def describe_resturant(self):
+        """x2 items of interest"""
+        print(self.name.title() + " is very busy on a friday night, and is a " 
+                                + self.cuisine.title() + " style eatery.")
+     
+    def open_resturant(self):
+        """Prints message indicating resturant is open"""
+        print(self.name.title() + " is now open!\n")
+
+a_resturant = Resturant("russo", "italian")
+a_resturant.describe_resturant()
+a_resturant.open_resturant()
+
+b_resturant = Resturant("chen", "japanese")
+b_resturant.describe_resturant()
+b_resturant.open_resturant()
+
+c_resturant = Resturant("knight's arms", "pub fare")
+c_resturant.describe_resturant()
+c_resturant.open_resturant()
