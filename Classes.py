3d95837ffa847df9568373a6e5a26f2ee8153648
Stephen Clements <mrevilg@gmail.com>
2019-07-03 18:29:15 -0500

    Instances as Attributes

--- Classes.py
+++ Classes.py
@@ -116,7 +116,32 @@ my_used_car.read_odometer()
 my_used_car.increment_odometer(100)
 my_used_car.read_odometer()
 
-# ---------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+
+class Battery():
+    """ A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describe the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approx " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+# -------------------------------------------------------------------------------------
 
 class ElectricCar(Car): # Example of inheritance
     """Represents aspect of the car, specific to electric vehicles."""
@@ -128,18 +153,18 @@ class ElectricCar(Car): # Example of inheritance
         Then initialize attributes specific to Elec car.
         """
         super().__init__(make, model, year,) # Calls parent __init__
-        self.battery_size = 70
+        self.battery = Battery()
     
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print("This car has a " + str(self.battery_size) + "-kwh battery.")
+        # def describe_battery(self):
+          #  """Print a statement describing the battery size."""
+           # print("This car has a " + str(self.battery_size) + "-kwh battery.")
 
     def fill_gas_tank(self): # If this were in the Parent (same name) you can override method
         """Electric cars don't typically have gas tanks."""
         print("This car doesn;t have a gas tank!")
 
+    
 my_tesla = ElectricCar('tesla', 'models s', 2016)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
-
-# -------------------------------------------------------------------------------
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
