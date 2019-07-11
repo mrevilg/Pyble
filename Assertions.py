735224cfd7699897207c29055ebf0f99b50637f9
Stephen Clements <mrevilg@gmail.com>
2019-07-11 16:23:42 -0500

    Assert Methods

--- Assertions.py
+++ Assertions.py
@@ -3,3 +3,11 @@
 # Assert statements are not to be used for Validating Data
 
 # Asserts Can be disabled globally by an intrp setting
+
+# Assert Methods avaliable in unittest Module:
+# assertEqual(a,b)
+# assertNotEqual(a,b)
+# assertTrue(x)
+# assertFalse(x)
+# assertIn(item, list)
+# assertNotIn(item, list)
