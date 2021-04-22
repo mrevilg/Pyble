e7f26e3f80c3ec4c7dd8bb1400dc99adf928d44c
Stephen Clements <mrevilg@gmail.com>
2021-04-21 21:16:44 -0500

    *args and **kwargs

--- Functions.py
+++ Functions.py
@@ -105,7 +105,37 @@ sub_func = getMathFunction('-')
 print(sub_func(10, 3))
 
 # Decorating a function
+def printMyName():
+    print("Stephen")
 
-# Decorators
+def title(printNameFunction):
+    def wrapper(): # Wrapper is wrapping def into another def
+        print("Mr Super")
+        printNameFunction()
+    return wrapper
+
+decoratedFunction = title(printMyName) # complicated
+
+decoratedFunction()
+
+# Decorators (easy version, not using complicated)
+
+@title
+def printMyName():
+    print("Stephen")
+
+printMyName()
 
 # Decorators w/ Parameters
+
+def title(printNameFunction):
+    def wrapper(*args, **kwargs): 
+        print("Mr Super")
+        printNameFunction(*args, **kwargs)
+    return wrapper
+
+@title
+def printMyName(name):
+    print(name)
+
+printMyName("Doffy")
